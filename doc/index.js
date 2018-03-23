62803416c29c4360d5e43f82ea5fee9785621c1e
nanzhui <sanjose37@163.com>
2018-03-22 20:52:42 -0700

    initial
    initial change

--- doc/index.js
+++ doc/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+
+
 app.use(express.static(path.join(__dirname, '_book')));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'book', 'index.html'))
